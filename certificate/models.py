deff740c1f3bb64a6420c466a885a981d14feb3c
Jay Prakash <unijay12@gmail.com>
2021-02-01 19:10:18 +0530

    created models for certificate and events handling
    certificate : this model will store all the data of the person who will get certificate
    events : this model will list the event took place in the clubs
    
    both models are connected with a forien key

---
+++ certificate/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+import uuid
+from rest_framework import serializers
+
+# Create your models here.
+class Event(models.Model):
+    content = models.TextField()
+    date_event = models.DateField()
+    date_result = models.DateField()
+    sign = models.ImageField(upload_to='signature/')
+
+class Certificate(models.Model):
+    event = models.ForeignKey(Event,on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    url_key = models.UUIDField(default=uuid.uuid4,unique=True,auto_created=True)
