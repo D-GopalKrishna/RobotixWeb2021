ced230dc6e2fec136b5d9689aadc9f28acc39ca8
Jay Prakash <unijay12@gmail.com>
2021-02-03 14:36:44 +0530

    post save signal created

--- certificate/models.py
+++ certificate/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 import uuid
 from rest_framework import serializers
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+import urllib.request
+from django.core.files import File
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from io import StringIO
+import os
 
+from .utill import imagedraw
 # Create your models here.
 class Event(models.Model):
     content = models.TextField()
@@ -14,3 +22,15 @@ class Certificate(models.Model):
     name = models.CharField(max_length=50)
     url_key = models.UUIDField(default=uuid.uuid4,unique=True,auto_created=True)
     image = models.ImageField(upload_to='certificate/',null=True,blank=True)
+
+@receiver(post_save,sender=Certificate)
+def img_handler(created,instance,*args,**kwargs):
+    if created:
+        reopen = imagedraw(instance.name)
+        tempfile_io = StringIO()
+        reopen.save(tempfile_io,format='JPEG')
+        length=tempfile_io.seek(0, os.SEEK_END)
+        image_file = InMemoryUploadedFile(tempfile_io, None, '{instance.name}.jpg','image/jpeg',str(length), None)
+        #img_file = File(reopen)
+        instance.image.save("{instance.name}.jpeg", image_file)
+        instance.save()
