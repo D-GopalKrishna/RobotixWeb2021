2fd711c3db9c19933f1bed731fc5b9f3e236623d
Ayush Ganguli <53971272+ayushganguli1769@users.noreply.github.com>
2021-02-03 19:52:07 +0530

    image extraction finally working

--- certificate/models.py
+++ certificate/models.py
@@ -8,9 +8,17 @@ from django.core.files import File
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import io
 import os
-
-from .utill import imagedraw
 from events.models import Event
+from .utill import imagedraw
+
+from PIL import Image
+import requests
+from io import BytesIO
+
+import requests
+import tempfile
+from django.core import files
+
 # Create your models here.
 
 class Certificate(models.Model):
@@ -18,15 +26,32 @@ class Certificate(models.Model):
     name = models.CharField(max_length=50)
     url_key = models.UUIDField(default=uuid.uuid4,unique=True,auto_created=True)
     image = models.ImageField(upload_to='certificate/',null=True,blank=True)
+    def __str__(self):
+        return self.name + "certificate for event " + self.event.title
+    
 
 @receiver(post_save,sender=Certificate)
 def img_handler(created,instance,*args,**kwargs):
     if created:
-        reopen = imagedraw(instance.name)
-        tempfile_io = io.BytesIO()
-        reopen.save(tempfile_io,format='JPEG')
-        #length=tempfile_io.seek(0, os.SEEK_END)
-        instance.image = InMemoryUploadedFile(tempfile_io, None, '{instance.name}.jpg','image/jpeg',tempfile_io.tell, None)
-        #img_file = File(reopen)
-        #.save("{instance.name}.jpeg", image_file)
-        instance.save()
+        image_url = "http://ayushganguli.pythonanywhere.com/static/profile.jpg"#replace this url in future
+        response = requests.get(image_url, stream=True)
+        if response.status_code != requests.codes.ok:
+            print("error")
+            instance.save()
+        else:
+            file_name = image_url.split('/')[-1]
+        
+            # Create a temporary file
+            lf = tempfile.NamedTemporaryFile()
+
+            # Read the streamed image in sections
+            for block in response.iter_content(1024 * 8):
+                
+                # If no more file then stop
+                if not block:
+                    break
+
+                # Write image block to temporary file
+                lf.write(block)
+            instance.image.save(file_name, files.File(lf))
+            instance.save()
